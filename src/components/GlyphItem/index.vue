985fcd823039e62e84add45e03fc9945e9a6cba4
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-16 11:33:17 +0100

    add copy to clipboard

--- src/components/GlyphItem/index.vue
+++ src/components/GlyphItem/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <button class="glyph-item">
+  <button
+    class="glyph-item"
+    data-focusable
+    @keydown.enter="copyToClipboard"
+    @dblclick="copyToClipboard"
+  >
     {{ glyph.symbol }}
   </button>
 </template>
 
 <script>
+import copy from 'copy-to-clipboard'
+
 export default {
   props: {
     glyph: {
       required: true,
       type: Object,
+    },
+  },
+
+  methods: {
+    copyToClipboard() {
+      const copied = copy(this.glyph.symbol)
+      if (copied) {
+        console.log('copied', this.glyph)
+      } else {
+        console.log('copy failed')
+      }
     },
   },
 }
