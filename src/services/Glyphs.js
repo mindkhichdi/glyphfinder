706d4f3471f7d5934c31452d3eb98f0701408024
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-16 15:27:02 +0100

    fix sorting

--- src/services/Glyphs.js
+++ src/services/Glyphs.js
@@ -19,66 +19,69 @@ export default new class {
       builder.field('alts')
       builder.field('description')
 
-      this.items.forEach(glyph => {
-        builder.add(glyph)
-      })
+      this.items.forEach(glyph => builder.add(glyph))
     })
   }
 
   buildItems(items) {
-    return items.map(glyph => {
-      const unfilteredTags = glyph.tags
-      const [tags, alts] = collect(unfilteredTags)
-        .partition(item => {
-          const isWord = /^\w{2,}$/.test(item) // min 2 letters
-          const isWordWithHyphens = /^((?:\w+-)+\w+)$/.test(item)
+    return collect(items)
+      .map(glyph => {
+        const unfilteredTags = glyph.tags
+        const [tags, alts] = collect(unfilteredTags)
+          .partition(item => {
+            const isWord = /^\w{2,}$/.test(item) // min 2 letters
+            const isWordWithHyphens = /^((?:\w+-)+\w+)$/.test(item)
 
-          return isWord || isWordWithHyphens
-        })
-        .toArray()
+            return isWord || isWordWithHyphens
+          })
+          .toArray()
 
-      return {
-        ...glyph,
-        unfilteredTags,
-        tags,
-        alts,
-      }
-    })
+        return {
+          ...glyph,
+          unfilteredTags,
+          tags,
+          alts,
+        }
+      })
+      .sortBy(glyph => glyph.symbol)
+      .toArray()
   }
 
   search(searchQuery = null) {
-    if (searchQuery === null) {
+    const filteredQuery = searchQuery ? searchQuery.toLowerCase().trim() : ''
+
+    if (filteredQuery === '') {
       return this.items
     }
 
     return this.idx
       .query(query => {
         // exact match
-        query.term(searchQuery, {
+        query.term(filteredQuery, {
           fields: ['symbol', 'entities', 'alts', 'tags'],
           boost: 10,
         })
 
         // first chars correct
-        query.term(searchQuery, {
+        query.term(filteredQuery, {
           fields: ['symbol', 'entities', 'tags'],
           boost: 5,
           wildcard: lunr.Query.wildcard.TRAILING,
         })
 
         // fuzzy
-        query.term(searchQuery, {
+        query.term(filteredQuery, {
           fields: ['symbol', 'entities', 'tags'],
           editDistance: 1,
         })
 
-        query.term(searchQuery, {
+        query.term(filteredQuery, {
           fields: ['description'],
           wildcard: lunr.Query.wildcard.TRAILING,
         })
       })
       .map(item => ({
-        ...data.find(glyph => glyph.symbol === item.ref),
+        ...this.items.find(glyph => glyph.symbol === item.ref),
         score: item.score,
       }))
   }
