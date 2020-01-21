737f69b05a8c9088edafa8d91de7e6aaf802a9cf
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-21 21:07:42 +0100

    add emoji skins
    Former-commit-id: 4e27169f8790491c6f0366b0c4dadcee999bbd0f

--- src/generator/generate.js
+++ src/generator/generate.js
@@ -45,19 +45,38 @@ function formatCodePoints(data, HTMLentities) {
     .toArray()
 
   const emojis = collect(rawEmojis)
-    .map(item => ({
-      symbol: item.emoji,
-      hex: item.hexcode.split('-').join(' '),
-      code: '',
-      name: item.annotation,
-      category: groups[item.group],
-      entities: '',
-      tags: [
+    .map(item => {
+
+      const tags = [
         'emoji',
         subgroups[item.subgroup],
         ...item.tags,
-      ].join(' '),
-    }))
+      ].join(' ')
+
+      return [
+        {
+          symbol: item.emoji,
+          hex: item.hexcode.split('-').join(' '),
+          code: '',
+          name: item.annotation,
+          category: groups[item.group],
+          entities: '',
+          tags,
+        },
+        ...(item.skins
+          ? item.skins.map(skin => ({
+            symbol: skin.emoji,
+            hex: skin.hexcode.split('-').join(' '),
+            code: '',
+            name: skin.annotation,
+            category: groups[skin.group],
+            entities: '',
+            tags,
+          })) : []
+        ),
+      ]
+    })
+    .flatten(1)
     .toArray()
 
   return [
