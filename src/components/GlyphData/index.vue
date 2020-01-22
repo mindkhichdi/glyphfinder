b19fbece04d4eac7f24a2997ac4062e3e677e5b7
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-22 15:45:34 +0100

    show name
    Former-commit-id: 041545a328cff4c3b04dee09bf6f47c6c9d80ed3

---
+++ src/components/GlyphData/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="glyph-data">
+    {{ glyph.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    glyph: {
+      required: true,
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
--- src/components/Navigatable/index.vue
+++ src/components/Navigatable/index.vue
@@ -8,14 +8,14 @@ export default {
   },
 
   data() {
-    const showRows = 8
+    const showRows = 7
 
     return {
       selectedIndex: 0,
       startRow: 0,
       showRows,
       itemsPerRow: 5,
-      rowHeight: 50,
+      rowHeight: 60,
       firstFullyVisibleRow: 0,
       lastFullyVisibleRow: showRows - 1,
     }
@@ -144,7 +144,9 @@ export default {
   },
 
   render() {
-    return this.$scopedSlots.default()
+    return this.$scopedSlots.default({
+      selectedGlyph: this.selectedGlyph,
+    })
   },
 }
 </script>
