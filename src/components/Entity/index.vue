7607adcf5c84a4703dc0b9b7f2d7400cfe3b92fa
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-24 17:04:51 +0100

    add some styling
    Former-commit-id: 8e9aace38d5f62144889d927a1032824d2b35215

---
+++ src/components/Entity/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="entity" @click="copyToClipboard">
+    {{ name }}
+  </button>
+</template>
+
+<script>
+import copy from 'copy-to-clipboard'
+
+export default {
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+
+  computed: {
+    fullName() {
+      return `&${this.name};`
+    },
+  },
+
+  methods: {
+    copyToClipboard() {
+      const copied = copy(this.fullName)
+
+      if (copied) {
+        console.log('copied', this.fullName)
+      } else {
+        console.log('copy failed')
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
