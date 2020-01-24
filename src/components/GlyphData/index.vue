c9139f1d2691aa86bff0efa1cf5993488d7c04a3
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-01-24 15:47:00 +0100

    add toggle
    Former-commit-id: 605542657d0b01085ae18792aa26d0ec39685d37

--- src/components/GlyphData/index.vue
+++ src/components/GlyphData/index.vue
@@ -1,23 +1,74 @@
 <template>
   <div class="glyph-data">
-    <div class="glyph-data__name">
-      {{ glyph.name }}
+    <div class="glyph-data__header">
+      <div class="glyph-data__name">
+        {{ glyph.name }}
+      </div>
+      <button
+        class="glyph-data__toggle"
+        type="button"
+        @click="isActive = !isActive"
+      >
+        <icon
+          class="glyph-data__icon"
+          :class="{ 'is-active': isActive }"
+          name="arrow-down"
+        />
+      </button>
     </div>
-    <div v-if="showShortcut">
-      Shortcut: {{ shortcut }}
-    </div>
+
+    <slide-up-down :active="isActive">
+      <div class="glyph-data__content">
+        <div class="glyph-data__item">
+          <div class="glyph-data__label">
+            Shortcut
+          </div>
+          <template v-if="showShortcut">
+            {{ shortcut }}
+          </template>
+          <template v-else>
+            –
+          </template>
+        </div>
+
+        <div class="glyph-data__item">
+          <div class="glyph-data__label">
+            Entities
+          </div>
+          <template v-if="entities.length">
+            {{ entities }}
+          </template>
+          <template v-else>
+            –
+          </template>
+        </div>
+      </div>
+    </slide-up-down>
   </div>
 </template>
 
 <script>
+import Icon from '@/components/Icon'
+import SlideUpDown from '@/components/SlideUpDown'
 import Shortcut from '@/services/Shortcut'
 
 export default {
+  components: {
+    Icon,
+    SlideUpDown,
+  },
+
   props: {
     glyph: {
       required: true,
       type: Object,
     },
+  },
+
+  data() {
+    return {
+      isActive: false,
+    }
   },
 
   computed: {
@@ -27,6 +78,10 @@ export default {
 
     showShortcut() {
       return this.shortcut && this.shortcut.length > 1
+    },
+
+    entities() {
+      return this.glyph.entities.match(/\S+/g) || []
     },
   },
 }
