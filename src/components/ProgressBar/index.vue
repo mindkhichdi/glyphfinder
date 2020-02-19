d19a91373665a983446315b102a81a1d72adfb17
Philipp Kühn <kontakt@philipp-kuehn.com>
2020-02-19 18:10:39 +0100

    add progress bar
    Former-commit-id: 259875281f8873c6a408e84b28c1e11414646c87

---
+++ src/components/ProgressBar/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="progress-bar">
+    <div class="progress-bar__bar" :style="`width: ${percentage}%`" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      default: 0,
+      type: Number,
+    },
+
+    max: {
+      default: 100,
+      type: Number,
+    },
+  },
+
+  computed: {
+    percentage() {
+      if (this.value === 0 && this.max === 0) {
+        return 0
+      }
+
+      return (100 / this.max) * this.value
+    },
+
+    markerPercentage() {
+      if (!this.marker) {
+        return 0
+      }
+
+      return this.between(this.marker, 0, 100)
+    },
+  },
+
+  methods: {
+    between(value, min, max) {
+      return (Math.min(max, Math.max(min, value)))
+    },
+  },
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
